7ae48c6e41a166170b690d06dc8171aa23eccac7
shreyamadiga123 <shreyamadiga123@gmail.com>
2021-10-30 04:55:21 +0000

    Init

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start 'rails'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
@@ -7,4 +9,14 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+# test/test_helper.rb
+require 'simplecov'
+
+# features/support/env.rb
+require 'simplecov'
+
+# .simplecov
+SimpleCov.start 'rails' do
+  # any custom configs like groups and filters can be here at a central place
 end
